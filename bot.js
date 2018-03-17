7ae46a6c9027f7d2822d18f4e7bfcfcece5691ba
Olle Thunberg <olle.thunberg03@gmail.com>
2018-03-17 13:51:06 +0100

    Databases & Live Feed
    Added mongodb like databse.
    Added live feed using socket.io
    
    Readme.md will be cooming soon!

--- bot.js
+++ bot.js
@@ -1,19 +1,39 @@
 const Kahoot = require('./export.js');
-const listen = require('socket.io').listen(8000).sockets;
+var listen = require('socket.io').listen(4000).sockets;
+var mongo = require('mongodb').MongoClient;
+
 var client = new Kahoot;
 
 console.log('Kahoot-Bot is now running.');
+ mongo.connect("mongodb://127.0.0.1/kahoot", function(err, db){
+        if (err) throw err;
+        console.log('Connected to the database.');
+        var database = db.db('kahoot');
+
 
     listen.on('connection', function(socket){
+        database.collection('feed').find().limit(10000).toArray(function(err, res){
+            if (err) throw err;
+           socket.emit('feed', res);
+           
+        });
+
         socket.on('submit', function(data){
             var num = data.number;
             var name = data.name;
+            
             var nameArray = [];
-
-                joinGame(name, num);
-                
+            database.collection('feed').insert({message: num}, function(num){
+                console.log('Inserted feed into database!');
+            });
+            
+            socket.emit('feed', {message: num});
+            socket.broadcast.emit('feed',{message: num});
+            joinGame(name, num);
         });
-    });
+
+       });
+    }); 
 var joinGameIndex = 0;
 function joinGame (name, num){
  client.join(num, name+joinGameIndex);        
@@ -22,7 +42,6 @@ function joinGame (name, num){
     joinGameIndex++;
     client.join(num, name+joinGameIndex);
     client.on("joined", () => {
-    console.log('Joined with the username: ' + name+joinGameIndex);
      });
 
      
